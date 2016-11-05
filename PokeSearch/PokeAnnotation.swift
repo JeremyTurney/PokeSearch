f6b8761000287702b54e658adf3b148386f56366
Jeremy Turney <turneyj@outlook.com>
2016-11-05 12:14:53 -0700

    first upload

---
+++ PokeSearch/PokeAnnotation.swift
@@ -0,0 +1,9 @@
+//
+//  PokeAnnotation.swift
+//  PokeSearch
+//
+//  Created by Jeremy Turney on 11/4/16.
+//  Copyright © 2016 Jeremy Turney. All rights reserved.
+//
+
+import Foundation
---
+++ PokeSearch/PokeSearch-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  PokeSearch-Bridging-Header.h
+//  PokeSearch
+//
+//  Created by Jeremy Turney on 11/1/16.
+//  Copyright © 2016 Jeremy Turney. All rights reserved.
+//
+
+#ifndef PokeSearch_Bridging_Header_h
+#define PokeSearch_Bridging_Header_h
+
+
+#endif /* PokeSearch_Bridging_Header_h */
